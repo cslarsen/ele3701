b82e8120f2e5d1c8a35b2efd212be3d1c9a85c87
Christian Stigen Larsen <csl@csl.name>
2015-05-08 11:30:23 +0200

    Adds ref

--- rapport/plots/landet.r
+++ rapport/plots/landet.r
@@ -1,6 +1,11 @@
-data <- read.csv("ssb.alarm.csv", encoding="UTF-8")
-print(data)
-summary(data)
+total <- read.csv("ssb.alarm.csv", encoding="UTF-8")
+nye <- read.csv("ssb.nye.csv", encoding="UTF-8")
+
+summary(total)
+summary(nye)
+
 pdf("alarmer-land.pdf")
-plot(data, type="b",
-     main="Mottakere av trygghetsalarmer i Norge")
+
+par(mfrow=c(2,2))
+plot(total, type="b", main="Mottakere av trygghetsalarm i Norge")
+plot(nye, type="b", main="Nye mottakere")
