78f0b8181dec9da4e80b3665f66086b431aed62b
Christian Stigen Larsen <csl@csl.name>
2015-05-08 11:56:33 +0200

    More

--- rapport/plots/landet.r
+++ rapport/plots/landet.r
@@ -1,11 +1,14 @@
-total <- read.csv("ssb.alarm.csv", encoding="UTF-8")
-nye <- read.csv("ssb.nye.csv", encoding="UTF-8")
+total <- read.csv("ssb.alarm.csv")
+nye <- read.csv("ssb.nye.csv")
+gjstr <- read.csv("ssb.gjstr.csv")
 
 summary(total)
 summary(nye)
+summary(gjstr)
 
 pdf("alarmer-land.pdf")
 
 par(mfrow=c(2,2))
-plot(total, type="b", main="Mottakere av trygghetsalarm i Norge")
+plot(total, type="b", main="Mottakere hele året")
 plot(nye, type="b", main="Nye mottakere")
+plot(gjstr, type="b", main="Mottakere hele eller deler av året")
