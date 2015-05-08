3fa5e60a190ae5825e987d239c706e3dbf384171
Christian Stigen Larsen <csl@csl.name>
2015-05-08 12:58:41 +0200

    Adds legend

--- rapport/plots/landet.r
+++ rapport/plots/landet.r
@@ -11,9 +11,16 @@ summary(gjstr)
 pdf("alarmer-land.pdf")
 
 par(mfrow=c(2,2))
-plot(total$År, total$Personer / 1000, xlab="År", ylab="Personer (tusen)",type="b", main="Mottakere hele året")
-plot(nye$År, nye$Personer / 1000, xlab="År", ylab="Personer (tusen)", type="b", main="Nye mottakere")
-plot(gjstr$År, gjstr$Personer / 1000, xlab="År", ylab="Personer (tusen)", type="b", main="Mottakere hele eller deler av året")
+
+plot(total$År, total$Personer / 1000, xlab="År", ylab="Personer
+     (tusen)",type="b", main="Mottakere hele året")
+
+plot(nye$År, nye$Personer / 1000, xlab="År", ylab="Personer (tusen)", type="b",
+     main="Nye mottakere")
+
+plot(gjstr$År, gjstr$Personer / 1000, xlab="År", ylab="Personer (tusen)",
+     type="b", main="Mottakere hele eller deler av året")
+
 #pie(c(5361,68070),labels=c("Yngre enn 67 år\n7%", "67 år og eldre\n93%"), main="Mottakere i 2013",col=c("lightgray","darkgray"))
 
 y <- utv$X0..66
@@ -23,4 +30,4 @@ yl = yl / 1000
 plot(utv$År, y / 1000, type="b", ylim=yl, ylab="Personer (tusen)", xlab="År",
      main="Aldersfordeling hjemmetjenester")
 lines(utv$År, o / 1000, type="b", ylim=yl, pch=3)
-print(yl)
+legend(2010,105,c(">= 67 år", "< 67 år"), pch=c(3,1))
