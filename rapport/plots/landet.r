95c6ccb5a872d9764231f4a69ef7e41e229fe233
Christian Stigen Larsen <csl@csl.name>
2015-05-08 12:45:00 +0200

    Shows thousands

--- rapport/plots/landet.r
+++ rapport/plots/landet.r
@@ -11,15 +11,16 @@ summary(gjstr)
 pdf("alarmer-land.pdf")
 
 par(mfrow=c(2,2))
-plot(total, type="b", main="Mottakere hele året")
-plot(nye, type="b", main="Nye mottakere")
-plot(gjstr, type="b", main="Mottakere hele eller deler av året")
+plot(total$År, total$Personer / 1000, xlab="År", ylab="Personer (tusen)",type="b", main="Mottakere hele året")
+plot(nye$År, nye$Personer / 1000, xlab="År", ylab="Personer (tusen)", type="b", main="Nye mottakere")
+plot(gjstr$År, gjstr$Personer / 1000, xlab="År", ylab="Personer (tusen)", type="b", main="Mottakere hele eller deler av året")
 #pie(c(5361,68070),labels=c("Yngre enn 67 år\n7%", "67 år og eldre\n93%"), main="Mottakere i 2013",col=c("lightgray","darkgray"))
 
 y <- utv$X0..66
 o <- utv$X67..
 yl = c(min(min(y), min(o)), max(max(y), max(o)))
-plot(utv$År, y, type="b", ylim=yl, ylab="Personer", xlab="År",
+yl = yl / 1000
+plot(utv$År, y / 1000, type="b", ylim=yl, ylab="Personer (tusen)", xlab="År",
      main="Aldersfordeling hjemmetjenester")
-lines(utv$År, o, type="b", ylim=yl, pch=3)
+lines(utv$År, o / 1000, type="b", ylim=yl, pch=3)
 print(yl)
